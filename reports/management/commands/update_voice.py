1b055bec2b38af2a4210b3639955efb23bb00af9
faithNassiwa <faithnassiwa@gmail.com>
2018-04-06 21:45:26 +0300

    Add art contact model

--- reports/management/commands/update_voice.py
+++ reports/management/commands/update_voice.py
@@ -5,8 +5,8 @@ from reports.models import Voice, ArtContact
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        Voice.get_data(project_name=['SMS Maama'])
-        self.stdout.write(self.style.SUCCESS('Successfully added project voice data'))
+        # Voice.get_data(project_name='SMS Maama')
+        # self.stdout.write(self.style.SUCCESS('Successfully added project voice data'))
         ArtContact.fetch_voice_art_contact_data()
         self.stdout.write(self.style.SUCCESS('Successfully added art contacts data'))
 
